4c5f51dfeaa527c820a6101467261523870b216f
Hank Tom <hanktom@gmail.com>
2016-05-25 20:57:56 +0800

    ListView與SimpleCursorAdapter

--- app/src/main/java/com/tom/contactapp/MainActivity.java
+++ app/src/main/java/com/tom/contactapp/MainActivity.java
@@ -10,10 +10,13 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CONTACTS = 1;
+    private ListView list;
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.listView);
         //
         int permission =
                 ActivityCompat.checkSelfPermission(this,
@@ -44,21 +48,28 @@ public class MainActivity extends AppCompatActivity {
         }else{
             readContacts();
         }
-        /*
-        while(c.moveToNext()){
-            int index = c.getColumnIndex(ContactsContract.Contacts._ID);
-            int id = c.getInt(index);
-            String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-            Log.d("Phone", id+"/"+name);
-
-        }*/
     }
 
     private void readContacts() {
         Log.d("Contacts", "readContacts");
         ContentResolver cr = getContentResolver();
         Cursor c = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
+        int[] to = {android.R.id.text1};
+        SimpleCursorAdapter adapter =
+                new SimpleCursorAdapter(this,
+                        android.R.layout.simple_list_item_1,
+                        c, from, to , 0);
 
+
+        list.setAdapter(adapter);
+        /*
+        while(c.moveToNext()) {
+            int index = c.getColumnIndex(ContactsContract.Contacts._ID);
+            int id = c.getInt(index);
+            String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            Log.d("Phone", id + "/" + name);
+        }*/
     }
 }
 
