aefdb85a32c626bba0bbf0d6ce1c74d17ff41fae
Hank Tom <hanktom@gmail.com>
2016-05-25 19:57:10 +0800

    要求危險權限

--- app/src/main/java/com/tom/contactapp/MainActivity.java
+++ app/src/main/java/com/tom/contactapp/MainActivity.java
@@ -1,28 +1,52 @@
 package com.tom.contactapp;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CONTACTS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
-        ContentResolver cr = getContentResolver();
-        Cursor c = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        int permission =
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_CONTACTS);
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    REQUEST_CONTACTS);
+        }else{
+            readContacts();
+        }
+
+
+        /*
         while(c.moveToNext()){
             int index = c.getColumnIndex(ContactsContract.Contacts._ID);
             int id = c.getInt(index);
             String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
             Log.d("Phone", id+"/"+name);
 
-        }
+        }*/
+
+    }
+
+    private void readContacts() {
+        ContentResolver cr = getContentResolver();
+        Cursor c = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+
 
     }
 }
